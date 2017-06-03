9fd205359322fb1e1d54abf19bc608baaaf25062
Dirk Rombauts <dirk.rombauts@questmaster.net>
2017-06-03 15:09:13 +0200

    Extract file encoding logic to a separate class

--- src/Pickles/Pickles/FileSystemBasedFeatureParser.cs
+++ src/Pickles/Pickles/FileSystemBasedFeatureParser.cs
@@ -32,10 +32,13 @@ namespace PicklesDoc.Pickles
 
         private readonly FeatureParser parser;
 
+        private readonly EncodingDetector encodingDetector;
+
         public FileSystemBasedFeatureParser(FeatureParser parser, IFileSystem fileSystem)
         {
             this.parser = parser;
             this.fileSystem = fileSystem;
+            this.encodingDetector = new EncodingDetector(this.fileSystem);
         }
 
         public Feature Parse(string filename)
@@ -69,19 +72,7 @@ namespace PicklesDoc.Pickles
 
         private Encoding GetEncoding(string filename)
         {
-            var bom = new byte[4];
-            using (var file = this.fileSystem.FileInfo.FromFileName(filename).OpenRead())
-            {
-                file.Read(bom, 0, 4);
-            }
-
-            // Analyze the BOM
-            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
-            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
-            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
-            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
-            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
-            return Encoding.UTF8;
+            return this.encodingDetector.GetEncoding(filename);
         }
     }
 }
